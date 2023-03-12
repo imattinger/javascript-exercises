2a2447fb3859e0e3ab8da048e1a9b439cb227d74
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-12 16:22:36 -0700

    Solve js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let letters = word.split('');
+    let left = 0;
+    let right = letters.length;
+    while (left < right) {
+        let temp = letters[left];
+        letters[left] = letters[right];
+        letters[right] = temp;
+        left++;
+        right--;
+    }
+    return letters.join('');
 };
 
 // Do not edit below this line
