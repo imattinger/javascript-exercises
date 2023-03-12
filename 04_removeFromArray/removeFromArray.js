6c8c0417e691486d598302a7c8c50c18b76dbc1f
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-12 16:48:35 -0700

    Solve js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    return arr.filter((elem)=>{return !toRemove.includes(elem)});
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*
+Previous solution: shows how Array.filter() works
+
+const removeFromArray = function(arr, ...toRemove) {
+    let filterFunction = function(element) {
+        return !toRemove.includes(element);
+    }
+    return arr.filter(filterFunction);
+};
+*/
