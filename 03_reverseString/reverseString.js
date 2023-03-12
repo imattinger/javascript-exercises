9e820e5d3126743e3a943becdf99c29957d85395
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-12 16:24:58 -0700

    Use cascading and built-in reverse() to solve js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,5 @@
 const reverseString = function(word) {
-    let letters = word.split('');
-    let left = 0;
-    let right = letters.length;
-    while (left < right) {
-        let temp = letters[left];
-        letters[left] = letters[right];
-        letters[right] = temp;
-        left++;
-        right--;
-    }
-    return letters.join('');
+    return word.split('').reverse().join('');
 };
 
 // Do not edit below this line
