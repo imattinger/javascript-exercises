f2381c32abe7c7b89b8e8e5147f90024877c6bcf
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-12 17:05:49 -0700

    Solve js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // Input check - type must be number
+    if (typeof(num1) != 'number' || typeof(num2) != 'number') 
+        return 'ERROR';
+    // Input check - nums can't be negative
+    if (num1 < 0 || num2 < 0) 
+        return 'ERROR';
+    // Using n(n+1)/2 is faster than iterating
+    let lowerBd = Math.min(num1, num2) - 1;
+    let upperBd = Math.max(num1, num2);
+    let sum1 = lowerBd * (lowerBd + 1) / 2;
+    let sum2 = upperBd * (upperBd + 1) / 2;
+    return sum2 - sum1;
 };
 
 // Do not edit below this line
