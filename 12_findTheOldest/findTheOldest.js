e514f4cd6e42880f735a6159c7adeaffd3b85744
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-30 19:07:58 -0700

    Solve js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestToYoungest = people.sort((a, b) => {
+        return getAge(a) > getAge(b) ? -1 : 1;
+    });
+    console.table(oldestToYoungest);
+    return oldestToYoungest[0];
 };
+
+function getAge(person) {
+    const birth = person.yearOfBirth;
+    const death = person.yearOfDeath || new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
