f385b5189eb720936f947fdd0cd54c13146d9a32
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-30 18:19:07 -0700

    Solve js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // Not the most JS-ish way to do things
+    let left = 0;
+    let right = word.length - 1;
+    while (left < right) {
+        // Skip non-alphabet characters
+        if (!word[left].match(/[a-z]/i)) {
+            left++;
+            continue;
+        }
+        if (!word[right].match(/[a-z]/i)) {
+            right--;
+            continue;
+        }
+        if (word[left].toLowerCase() != word[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
