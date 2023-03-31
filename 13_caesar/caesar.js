e480f7eb4b7d0adcf3ccc5d8dc37cbe0221ed38c
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-30 19:35:32 -0700

    Solve js exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,19 @@
-const caesar = function() {
-
+const caesar = function(word, shift) {
+    return word.split('').map(letter => shiftLetter(letter, shift)).join('');
 };
+
+function shiftLetter(letter, shift) {
+    let ascVal = letter.charCodeAt();
+    let shiftedVal = ascVal;
+    shift = (shift % 26 + 26) % 26;  // handle negative shifts
+    if (ascVal >= 65 && ascVal <= 90) {
+        shiftedVal = 65 + ((ascVal - 65 + shift) % 26);
+    }
+    if (ascVal >= 97 && ascVal <= 122) {
+        shiftedVal = 97 + ((ascVal - 97 + shift) % 26);
+    }
+    return String.fromCharCode(shiftedVal);
+}
 
 // Do not edit below this line
 module.exports = caesar;
