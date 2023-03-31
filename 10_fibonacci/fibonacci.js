f70dce262e44f7aed297e017f3c4a71bdea7a2fe
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-30 18:30:04 -0700

    Solve js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    return fib(n, 0, 1);
 };
+
+function fib(n, a, b) {
+    if (n === 0) {
+        return a;
+    }
+    if (n === 1) {  
+        return b;
+    }
+    return fib(n - 1, b, a + b);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
